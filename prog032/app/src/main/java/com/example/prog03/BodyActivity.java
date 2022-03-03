124ca8197adc98f4aa31817d9b05e5f1d2f663b8
Ajay Singh <ajaysingh@berkeley.edu>
2022-03-02 16:01:14 -0800

    finished info page probably

---
+++ prog032/app/src/main/java/com/example/prog03/BodyActivity.java
@@ -0,0 +1,48 @@
+package com.example.prog03;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class BodyActivity extends AppCompatActivity {
+    EditText feet;
+    EditText inches;
+    EditText sex;
+    EditText weight;
+    EditText age;
+    Button save;
+    Button back;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        feet = findViewById(R.id.Feet);
+        inches = findViewById(R.id.Inches);
+        sex = findViewById(R.id.Sex);
+        weight = findViewById(R.id.Weight);
+        age = findViewById(R.id.Age);
+        save = findViewById(R.id.SaveButton);
+        back = findViewById(R.id.BackButton);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                feet.setHint(feet.getText());
+                inches.setHint(inches.getText());
+                sex.setHint(sex.getText());
+                weight.setHint(weight.getText());
+                age.setHint(age.getText());
+            }
+        });
+    }
+
+}
