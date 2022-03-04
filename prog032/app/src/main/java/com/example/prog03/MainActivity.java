451ded1b5ec681e462ebfe62296f548e825fa210
MaxwellLiu12 <mymaxliu@gmail.com>
2022-03-04 15:55:01 -0800

    added transitions between pages

--- prog032/app/src/main/java/com/example/prog03/MainActivity.java
+++ prog032/app/src/main/java/com/example/prog03/MainActivity.java
@@ -1,14 +1,47 @@
 package com.example.prog03;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+    private ImageButton exerciseIcon, bodyDetailsIcon;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        exerciseIcon = (ImageButton) findViewById(R.id.exerciseIcon);
+        bodyDetailsIcon = (ImageButton) findViewById(R.id.bodyDetailsIcon);
+
+        exerciseIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToAddExercise();
+            }
+        });
+
+        bodyDetailsIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToBodyDetails();
+            }
+        });
+
+    }
+    private void goToAddExercise() {
+
+        Intent intent = new Intent(this, ChooseActivity.class);
+        startActivity(intent);
+    }
+    private void goToBodyDetails() {
+
+        Intent intent = new Intent(this, BodyActivity.class);
+        startActivity(intent);
     }
 }
