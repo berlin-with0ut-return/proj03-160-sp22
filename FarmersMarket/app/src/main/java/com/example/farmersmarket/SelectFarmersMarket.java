a86f2537740e4944316304bbceac0ea9e3ab60ef
Aaron Lee <aaron@kali.local>
2022-04-19 22:22:03 -0700

    add select farmer page

--- FarmersMarket/app/src/main/java/com/example/farmersmarket/SelectFarmersMarket.java
+++ FarmersMarket/app/src/main/java/com/example/farmersmarket/SelectFarmersMarket.java
@@ -3,6 +3,8 @@ package com.example.farmersmarket;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class SelectFarmersMarket extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class SelectFarmersMarket extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_farmers_market);
+
+        Spinner dropdown = findViewById(R.id.spinner);
+        String[] items = new String[]{"South Berkeley - Tuesday", "North Berkeley - Thursday", "Downtown Berkeley - Saturday"};
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
+        dropdown.setAdapter(adapter);
     }
+
+
 }
