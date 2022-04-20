a3054d21fd15e2cff104b37e4c5d1029b242448d
Ajay Singh <ajaysingh@berkeley.edu>
2022-04-19 19:26:06 -0700

    done

--- FarmersMarket/app/src/main/java/com/example/farmersmarket/Account.java
+++ FarmersMarket/app/src/main/java/com/example/farmersmarket/Account.java
@@ -2,13 +2,41 @@ package com.example.farmersmarket;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Account extends AppCompatActivity {
+    Button logOut;
+    Button back;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_account);
+        setContentView(R.layout.activity_log_in);
+
+        logOut = (Button) findViewById(R.id.LogOutButton);
+
+        back = (Button) findViewById(R.id.BackButton);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+
+        logOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToHomeScreen();
+            }
+        });
+
+    }
+
+    private void goToHomeScreen() {
+        Intent intent = new Intent(this, HomeScreen.class);
+        startActivity(intent);
     }
 }
--- FarmersMarket/app/src/main/java/com/example/farmersmarket/LogIn.java
+++ FarmersMarket/app/src/main/java/com/example/farmersmarket/LogIn.java
@@ -2,13 +2,55 @@ package com.example.farmersmarket;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LogIn extends AppCompatActivity {
+
+    Button logIn;
+    Button signUp;
+    Button back;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+
+        logIn = (Button) findViewById(R.id.LogInButton);
+        signUp = (Button) findViewById(R.id.SignInButton);
+
+        back = (Button) findViewById(R.id.BackButton);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+
+        logIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToHomeScreen();
+            }
+        });
+
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToSignUp();
+            }
+        });
+
+    }
+
+    private void goToSignUp() {
+        Intent intent = new Intent(this, SignUp.class);
+        startActivity(intent);
+    }
+    private void goToHomeScreen() {
+        Intent intent = new Intent(this, HomeScreen.class);
+        startActivity(intent);
     }
 }
