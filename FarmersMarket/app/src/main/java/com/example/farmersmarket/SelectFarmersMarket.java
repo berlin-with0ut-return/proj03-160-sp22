0fe3460a5887301b38c19df76c31559f3a71545f
cerebrus1001 <darkrai1001@yahoo.com>
2022-04-21 20:29:42 -0700

    fix choose stand page and select farmers market page

--- FarmersMarket/app/src/main/java/com/example/farmersmarket/SelectFarmersMarket.java
+++ FarmersMarket/app/src/main/java/com/example/farmersmarket/SelectFarmersMarket.java
@@ -1,13 +1,17 @@
 package com.example.farmersmarket;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.TextView;
 
-public class SelectFarmersMarket extends AppCompatActivity {
-
+public class SelectFarmersMarket extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
+    TextView newview;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,7 +22,51 @@ public class SelectFarmersMarket extends AppCompatActivity {
 
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
         dropdown.setAdapter(adapter);
+        dropdown.setOnItemSelectedListener(this);
+        newview = findViewById(R.id.marketinfo);
+
     }
 
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View v, int position, long id) {
+        String text;
+        switch (position) {
+            case 0:
+                displayMap(0);
+                text = "Hours and Location: \nAdeline Street and 63rd Street \nTuesdays 2 pm - 6:30 pm year-round";
+                createNewTextView(text);
+                break;
+            case 1:
+                displayMap(1);
+                text = "Hours and Location: \nShattuck Avenue @ Vine Street \nThursdays 3 pm - 7 pm year-round";
+                createNewTextView(text);
+                break;
+            case 2:
+                displayMap(2);
+                text = "Hours and Location: \nCenter Street @ M. L. King, Jr. Way \nSaturdays 10 am - 3 pm year-round";
+                createNewTextView(text);
+                break;
+
+        }
+    }
+
+    private void createNewTextView(String text) {
+//        final ConstraintLayout.LayoutParams lparams = new ConstraintLayout.LayoutParams(
+//                ConstraintLayout.LayoutParams.WRAP_CONTENT,
+//                ConstraintLayout.LayoutParams.WRAP_CONTENT);
+//        final TextView textView = new TextView(this);
+//        newview.setLayoutParams(lparams);
+        newview.setText(text);
+//        return textView;
+    }
+
+    private void displayMap(int selection) {
+        // TODO (MAX)
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+        // Do nothing
+    }
 
 }
