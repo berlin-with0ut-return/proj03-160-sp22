21d13b748b809acfe77951b80236312f17607eeb
MaxwellLiu12 <mymaxliu@gmail.com>
2022-04-25 19:35:51 -0700

    Added in button functionality for details and review

--- FarmersMarket/app/src/main/java/com/example/farmersmarket/ProductDetail.java
+++ FarmersMarket/app/src/main/java/com/example/farmersmarket/ProductDetail.java
@@ -2,7 +2,10 @@ package com.example.farmersmarket;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ProductDetail extends AppCompatActivity {
 
@@ -10,5 +13,31 @@ public class ProductDetail extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_product_detail);
+
+        Button button = (Button) findViewById(R.id.back_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openChooseStand();
+            }
+        });
+
+        Button button1 = (Button) findViewById(R.id.review_button);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openReview();
+            }
+        });
+    }
+
+    public void openReview() {
+        Intent intent = new Intent(this, Review.class);
+        startActivity(intent);
+    }
+
+    public void openChooseStand() {
+        Intent intent = new Intent(this, ChooseStand.class);
+        startActivity(intent);
     }
 }
--- FarmersMarket/app/src/main/java/com/example/farmersmarket/Review.java
+++ FarmersMarket/app/src/main/java/com/example/farmersmarket/Review.java
@@ -2,7 +2,10 @@ package com.example.farmersmarket;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Review extends AppCompatActivity {
 
@@ -10,5 +13,27 @@ public class Review extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_review);
+
+        Button button = (Button) findViewById(R.id.back_button3);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openProductDetail();
+            }
+        });
+
+        Button button1 = (Button) findViewById(R.id.submit_button);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openProductDetail();
+            }
+        });
+
+    }
+
+    public void openProductDetail() {
+        Intent intent = new Intent(this, ProductDetail.class);
+        startActivity(intent);
     }
 }
